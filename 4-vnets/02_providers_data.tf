f67dd1a7c478bdfb7b538402f5a50a3c20fa9d0a
lenvolk <lvolkov@outlook.com>
2020-01-23 14:36:28 +0000

    test1

--- 4-vnets/02_providers_data.tf
+++ 4-vnets/02_providers_data.tf
@@ -2,24 +2,24 @@
 # PROVIDERS
 #############################################################################
 
-provider "azurerm" {
-  subscription_id = var.subscription_id
-  client_id       = var.client_id
-  client_secret   = var.client_secret
-  tenant_id       = var.tenant_id
-  version        = "~> 1.0" 
-  #alias          = "networking"
-}
-
 # provider "azurerm" {
-#   subscription_id = "25de1ca2-09a3-42e0-97cc-5fffbc53286f"  #var.mainsub_id
-#   #subscription_id = data.azurerm_subscription.current.subscription_id
+#   subscription_id = var.subscription_id
 #   client_id       = var.client_id
 #   client_secret   = var.client_secret
 #   tenant_id       = var.tenant_id
 #   version        = "~> 1.0" 
-#   alias          = "security"
+#   alias          = "hubnet"
 # }
+
+provider "azurerm" {
+  subscription_id = var.submsdn_id
+  #subscription_id = data.azurerm_subscription.current.subscription_id
+  client_id       = var.client_id
+  client_secret   = var.client_secret
+  tenant_id       = var.tenant_id
+  version        = "~> 1.0" 
+  #alias          = "spokenet"
+}
 
 #Set the terraform backend
 terraform {
@@ -32,9 +32,9 @@ terraform {
 # DATA
 #############################################################################
 
-data "azurerm_subscription" "current" {
-  #provider = azurerm.networking
-}
+# data "azurerm_subscription" "current" {
+#       provider = azurerm.hubnet
+# }
 
 # data "azurerm_resource_group" "my_rg" {
 #   #provider = azurerm.networking
--- 4-vnets/03_modules.tf
+++ 4-vnets/03_modules.tf
@@ -1,13 +1,18 @@
 #Call module
 module "Az-VirtualNetwork-Demo" {
   source                      = "git::https://github.com/lenvolk/terraform-azurerm-Az-VirtualNetwork.git//?ref=dev"
-  net_prefix                  = "myproduct-perimeter"
+  net_prefix                  = "00-spokenet"
   network_resource_group_name = "ps-vnets"
   virtual_networks            = var.virtual_networks
   subnets                     = var.subnets
-  route_tables                = var.route_tables
-  network_security_groups     = var.network_security_groups
-  pips                        = var.pips
-  vnets_to_peer               = var.vnets_to_peer
+  #route_tables                = var.route_tables
+  #network_security_groups     = var.network_security_groups
+  #pips                        = var.pips
+  #vnets_to_peer               = var.vnets_to_peer
   net_additional_tags         = var.net_additional_tags
+
+  # providers        = {
+  # azurerm          = azurerm.spokenet
+  # }
+
 }
